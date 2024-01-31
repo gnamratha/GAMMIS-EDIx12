df9ca8583521ed1f6aadb1fa3cfc0da5ad78edbc
gnamratha <153008252+gnamratha@users.noreply.github.com>
2024-01-31 10:56:06 -0500

    Delete src directory

--- src/main/java/com/edi/sample/Main.java
+++
@@ -1,8 +0,0 @@
-//package com.edi;
-//
-//public class Main {
-//    // HelloWorld
-//        public static void main(String[] args) {
-//            System.out.println("Hello, World!");
-//        }
-//}
