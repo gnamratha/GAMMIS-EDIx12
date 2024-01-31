a7ce887d9c5647fad0262d898a67b2e3bd3b0a93
gnamratha <153008252+gnamratha@users.noreply.github.com>
2024-01-31 10:58:22 -0500

    Add files via upload

---
+++ EDI837P/src/main/java/com/edi/JsonToEdiApplication_837P.java
@@ -0,0 +1,11 @@
+package com.edi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonToEdiApplication_837P {
+
+    public static void main(String[] args) {
+        SpringApplication.run( JsonToEdiApplication_837P.class, args);
+    }
+}
